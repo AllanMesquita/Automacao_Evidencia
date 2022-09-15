283991ced0248d7f15de65589a947cf08d8df702
AllanMesquita <72563007+AllanMesquita@users.noreply.github.com>
2022-09-15 17:04:11 -0300

    Update

--- Test_Zone/bd_nfItens.py
+++ Test_Zone/bd_nfItens.py
@@ -20,9 +20,6 @@ data = open("C:\\Users\\allan.mesquita\\Downloads\\teste.json")
 
 req_body = json.load(data)
 
-cursor.close()
-conn.close()
-
 for dic in req_body:
 
     chave_acesso = dic['Chave de Acesso']
@@ -54,3 +51,37 @@ for dic in req_body:
     aliq_ipi = dic['AlÃ\xadq_x002e_ IPI']
     ncm = dic['NCM']
     cfop = dic['CFOP']
+
+    # PESQUISA NOTA NO BANCO
+    cursor.execute(f"SELECT chave_acesso FROM public.nf_entrada_itens2 WHERE chave_acesso = '{chave_acesso}'")
+    resultado = cursor.fetchall()
+    if bool(resultado):
+        continue
+    else:
+    # PESQUISAS
+        # CFOP
+        teste = pesquisa(cursor, conn, chave_acesso, dic)
+        id_cfop = teste.cfop()
+        print(id_cfop)
+
+    cursor.execute(
+        'INSERT INTO public.nf_entraa_itens2 ('
+        'chave_acesso, data_emissao, descricao_produto, cod_produto, numero_pedido, cean, cean_trib, unid_com, '
+        'valor_unitario, quantidade_com, valor_total, origem, base_calculo_icms, cst_icms_csosn, aliq_icms, valor_icms,'
+        'perc_margem_icms_st, base_calc_icms_st, valor_icms_st, aliq_icms_st, valor_pis, cst_pis, valor_cofins, '
+        'cst_cofins, valor_ipi, cst_ipi, aliq_ipi, ncm, cfop'
+        ')'
+        'VALUES ('
+        '%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, '
+        '%s, %s'
+        ')',
+        (
+            chave_acesso, data_emissao, descricao_produto, cod_produto, numero_pedido, cean, cean_trib, unid_com,
+            valor_unitario, quantidade, valor_total, origem, base_icms, cst_icms, aliq_icms, valor_icms,
+            perc_icms, base_icms_st, valor_icms_st, aliq_icms_st, valor_pis, cst_pis, valor_cofins,
+            cst_cofins, valor_ipi, cst_ipi, aliq_ipi, ncm, id_cfop
+        )
+    )
+
+cursor.close()
+conn.close()
