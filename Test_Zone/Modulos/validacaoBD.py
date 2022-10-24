f7c7d0364bd198e3515c9d671b4672469c525c5b
AllanMesquita <72563007+AllanMesquita@users.noreply.github.com>
2022-10-24 15:01:06 -0300

    Update

--- Test_Zone/Modulos/validacaoBD.py
+++ Test_Zone/Modulos/validacaoBD.py
@@ -5,8 +5,7 @@ repeticao_RFID = {}
 repeticao_SN = {}
 
 
-
-def rec_validation(item, repeticao):
+def rec_validation(lista, repeticao):
     # Imports
     from datetime import datetime
     from Modulos.class_erros import Error, SaveError
@@ -28,140 +27,142 @@ def rec_validation(item, repeticao):
 
     ### VALIDAÇÃO DA CHAVE DE NOTA FISCAL
 
-    cell_range = item['ChaveNF_Entrada']
-    if bool(cell_range) is False:
-        error.empty()
-        error_chave += 1
-    try:
-        cell_range = int(cell_range)
-    except:
-        error.chave()
-        error_chave += 1
-    finally:
-        pass
-    if len(cell_range) != 44:
-        error.chave()
-        error_chave += 1
+    for item in lista:
 
-    ### VALIDAÇÃO DO PEDIDO DE COMPRA (PO)
+        cell_range = item['ChaveNF_Entrada']
+        if bool(cell_range) is False:
+            error.empty()
+            error_chave += 1
+        try:
+            cell_range = int(cell_range)
+        except:
+            error.chave()
+            error_chave += 1
+        finally:
+            pass
+        if len(cell_range) != 44:
+            error.chave()
+            error_chave += 1
 
-    cell_range = item['PedidoCompra']
-    if bool(cell_range) is False:
-        error_PO += 1
-        error.empty()
-    try:
-        int_test = int(cell_range[1:])
-    except:
-        error.po()
-        error_PO += 1
-    finally:
-        pass
-    if 'K' in cell_range or 'k' in cell_range:
-        if len(cell_range[1:]) > 5 or len(cell_range[1:]) < 5:
+        ### VALIDAÇÃO DO PEDIDO DE COMPRA (PO)
+
+        cell_range = item['PedidoCompra']
+        if bool(cell_range) is False:
+            error_PO += 1
+            error.empty()
+        try:
+            int_test = int(cell_range[1:])
+        except:
+            error.po()
+            error_PO += 1
+        finally:
+            pass
+        if 'K' in cell_range or 'k' in cell_range:
+            if len(cell_range[1:]) > 5 or len(cell_range[1:]) < 5:
+                error_PO += 1
+                error.po()
+        elif len(str(cell_range)) > 5 or len(str(cell_range)) < 5:
             error_PO += 1
             error.po()
-    elif len(str(cell_range)) > 5 or len(str(cell_range)) < 5:
-        error_PO += 1
-        error.po()
-    else:
-        pass
-
-    ### VALIDAÇÃO DO PART-NUMBER
-
-    cell_range = item['PartNumber']
-    if bool(cell_range) is False or cell_range is None:
-        error.empty()
-        error_PN += 1
-    elif "!" in cell_range or \
-            "@" in cell_range or \
-            "$" in cell_range or \
-            "%" in cell_range or \
-            "&" in cell_range or \
-            "*" in cell_range or \
-            ")" in cell_range or \
-            "'" in cell_range or \
-            ":" in cell_range or \
-            ";" in cell_range:
-        error.part_number()
-        error_PN += 1
-    else:
-        pass
-
-    ### VALIDAÇÃO RFID DO PRODUTO
-
-    cell_range = item['RFID_Produto']
-    if bool(cell_range) is False:
-        error.empty()
-        error_RFID += 1
-    elif len(cell_range) != 24:
-        error.rfid()
-        error_RFID += 1
-    elif "E" != cell_range[0]:
-        error.rfid()
-        error_RFID += 1
-    if repeticao.count(cell_range) > 1:
-        error.rfid_repetido()
-        error_RFID += 1
-    else:
-        pass
-
-    ### VALIDAÇÃO DO SERIAL NUMBER
-
-    cell_range = item['SerialNumber']
-    if bool(cell_range) is False:
-        error.empty()
-        error_SN += 1
-    if "!" in str(cell_range) or \
-            "@" in str(cell_range) or \
-            "$" in str(cell_range) or \
-            "%" in str(cell_range) or \
-            "&" in str(cell_range) or \
-            "*" in str(cell_range) or \
-            "(" in str(cell_range) or \
-            ")" in str(cell_range) or \
-            "'" in str(cell_range) or \
-            ":" in str(cell_range) or \
-            "/" in str(cell_range):
-        error.serial_number()
-        error_SN += 1
-    if repeticao.count(cell_range) > 1:
-        error.rfid_repetido()
-        error_RFID += 1
-    else:
-        pass
-
-    ### VALIDAÇÃO LOCAL
-
-    ### VALIDAÇÃO DA DATA
-
-    cell_range = item['DataEvidencia']
-
-    try:
-        parse(cell_range)
-        data = parse(cell_range)
-        if data.day <= 12:
-            data = datetime.strptime(datetime.strftime(parse(cell_range), "%m/%d/%Y"), "%d/%m/%Y")
-        if data > datetime.today():
-            error.data_maior()
-            error_Date += 1
         else:
             pass
-    except Exception as erros:
-        print(erros)
-        error.data()
-        error_Date += 1
-    finally:
-        pass
 
-    ### CHAVE DE RELACIONAMENTO
+        ### VALIDAÇÃO DO PART-NUMBER
 
-    # Select na tabela Recebimento com base na data
+        cell_range = item['PartNumber']
+        if bool(cell_range) is False or cell_range is None:
+            error.empty()
+            error_PN += 1
+        elif "!" in cell_range or \
+                "@" in cell_range or \
+                "$" in cell_range or \
+                "%" in cell_range or \
+                "&" in cell_range or \
+                "*" in cell_range or \
+                ")" in cell_range or \
+                "'" in cell_range or \
+                ":" in cell_range or \
+                ";" in cell_range:
+            error.part_number()
+            error_PN += 1
+        else:
+            pass
 
-    repeticao_RFID.clear()
-    repeticao_SN.clear()
+        ### VALIDAÇÃO RFID DO PRODUTO
 
-    if error_chave > 0 or error_PO > 0 or error_PN > 0 \
-            or error_RFID > 0 or error_SN > 0 or error_Date > 0 or error_ChaveRel:
-        return 'Erro nos dados'
-    else:
-        return 'Sucesso'
+        cell_range = item['RFID_Produto']
+        if bool(cell_range) is False:
+            error.empty()
+            error_RFID += 1
+        elif len(cell_range) != 24:
+            error.rfid()
+            error_RFID += 1
+        elif "E" != cell_range[0]:
+            error.rfid()
+            error_RFID += 1
+        if repeticao.count(cell_range) > 1:
+            error.rfid_repetido()
+            error_RFID += 1
+        else:
+            pass
+
+        ### VALIDAÇÃO DO SERIAL NUMBER
+
+        cell_range = item['SerialNumber']
+        if bool(cell_range) is False:
+            error.empty()
+            error_SN += 1
+        if "!" in str(cell_range) or \
+                "@" in str(cell_range) or \
+                "$" in str(cell_range) or \
+                "%" in str(cell_range) or \
+                "&" in str(cell_range) or \
+                "*" in str(cell_range) or \
+                "(" in str(cell_range) or \
+                ")" in str(cell_range) or \
+                "'" in str(cell_range) or \
+                ":" in str(cell_range) or \
+                "/" in str(cell_range):
+            error.serial_number()
+            error_SN += 1
+        if repeticao.count(cell_range) > 1:
+            error.rfid_repetido()
+            error_RFID += 1
+        else:
+            pass
+
+        ### VALIDAÇÃO LOCAL
+
+        ### VALIDAÇÃO DA DATA
+
+        cell_range = item['DataEvidencia']
+
+        try:
+            parse(cell_range)
+            data = parse(cell_range)
+            if data.day <= 12:
+                data = datetime.strptime(datetime.strftime(parse(cell_range), "%m/%d/%Y"), "%d/%m/%Y")
+            if data > datetime.today():
+                error.data_maior()
+                error_Date += 1
+            else:
+                pass
+        except Exception as erros:
+            print(erros)
+            error.data()
+            error_Date += 1
+        finally:
+            pass
+
+        ### CHAVE DE RELACIONAMENTO
+
+        # Select na tabela Recebimento com base na data
+
+        repeticao_RFID.clear()
+        repeticao_SN.clear()
+
+        if error_chave > 0 or error_PO > 0 or error_PN > 0 \
+                or error_RFID > 0 or error_SN > 0 or error_Date > 0 or error_ChaveRel:
+            return 'Erro nos dados'
+        else:
+            return 'Sucesso'
--- Test_Zone/QualidadeBD.py
+++ Test_Zone/QualidadeBD.py
@@ -1,14 +1,31 @@
 import json
 
 repeticao = []
+erros = []
+validacao = []
 
 data = open("C:\\Users\\allan.mesquita\\Downloads\\teste.json")
 
 obj = json.load(data)
 
 for item in obj:
-    repeticao.append(item['RFID_Produto'])
-    print(item)
+    if item['ChaveNF_Entrada'] not in repeticao:
+        repeticao.append(item['ChaveNF_Entrada'])
+    else:
+        pass
+    print(item['ChaveNF_Entrada'])
 
-if repeticao.count('E00000000000000000314981') > 1:
-    print('Teste')
+for nf in repeticao:
+    for item in obj:
+        if nf == item['ChaveNF_Entrada']:
+            validacao.append(item)
+        else:
+            pass
+    print(validacao)
+    for linha in validacao:
+        print(linha)
+    'Chama código validação'
+    'processo se erro'
+    validacao.clear()
+
+print(repeticao)
